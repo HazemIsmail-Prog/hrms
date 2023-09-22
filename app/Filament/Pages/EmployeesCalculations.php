49bfdf7284ef127af9508d31cd0ae19fa2fcc598
HazemIsmail-Prog <67516349+HazemIsmail-Prog@users.noreply.github.com>
2023-09-22 11:48:24 +0300

    rename employees calculations page

--- app/Filament/Pages/EmployeesCalculations.php
+++ app/Filament/Pages/EmployeesCalculations.php
@@ -4,7 +4,7 @@ namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
 
-class EmployeeCustomPage extends Page
+class EmployeesCalculations extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-calculator';
 
@@ -15,6 +15,6 @@ class EmployeeCustomPage extends Page
     protected static ?string $navigationGroup = 'Reports';
 
 
-    protected static string $view = 'filament.pages.employee-custom-page';
+    protected static string $view = 'filament.pages.employees-calculations-page';
 
 }
